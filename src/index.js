e2382e74a77514c321866beab5f7d703976506c1
Facaller <ashwin.metal13@gmail.com>
2025-07-16 13:41:39 +0200

    Flesh out fetchData method, add getWeatherData method

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./styles.css"
 import { apiScript } from "./weatherApi";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const api = new apiScript();
+});
--- src/weatherApi.js
+++ src/weatherApi.js
@@ -2,11 +2,30 @@ export class apiScript {
     constructor () {
         this.apiKey  = '6SZ6Y2GHCYFLUKC6VLHMLFPDS';
         this.baseUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+        this.location = 'cape town';
+        this.weatherData = {};
+
+        this.fetchData('cape town')
     }
 
     async fetchData (location) {
-        const apiKey = this.apiKey;
-        const baseUrl = this.baseUrl;
-        const url = `${baseUrl}` //add elements together
+        try {
+            const apiKey  = this.apiKey;
+            const baseUrl = this.baseUrl;
+            const url     = `${baseUrl}${location}?key=${apiKey}`;
+    
+            const response = await fetch(url);
+            const data     = await response.json();
+            console.log(data);
+            return data;
+        } catch (error) {
+            console.log(error)
+            return null;
+        }
+    }
+    // next steps is to create method to extract data I need and return it as object.
+    // Then create smaller methods that pull a specific data point from that object.
+    async getWeatherData () {
+        const data = await this.fetchData(this.location);
     }
 }
